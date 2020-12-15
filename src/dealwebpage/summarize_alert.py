daadd39bd97edd73443c0810e7d83951690129eb
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-12-15 18:46:56 +0900

    Fix: receive_alert thread's queue

--- src/dealwebpage/summarize_alert.py
+++ src/dealwebpage/summarize_alert.py
@@ -1,21 +1,18 @@
 from __future__ import annotations
 
-from collections import deque
 from logging import getLogger
 from multiprocessing import Queue
 from os import mkdir, path
-from threading import Event, Thread
-from typing import Union, cast
+from queue import Queue as threadQueue
+from threading import Thread
+from typing import Any, Union, cast
 
 from utils.alert_data import Alert
 from utils.file_rw import w_file
 
-data_list: deque[Union[str, Alert]] = deque()
-event = Event()
-
 logger = getLogger(__name__)
 
-def receive_alert(recv_q: Queue[Union[str, Alert]]):
+def receive_alert(recv_q: Queue[Union[str, Alert]], data_list: threadQueue[Any]):
     """
     他プロセスからのアラートデータを受信するスレッド
 
@@ -24,8 +21,7 @@ def receive_alert(recv_q: Queue[Union[str, Alert]]):
     """
     while True:
         recv_data = recv_q.get(block=True)
-        data_list.append(recv_data)
-        event.set()
+        data_list.put(recv_data)
         if recv_data == 'end':
             # 終了処理
             logger.info("Summarize alert: Receive end")
@@ -48,41 +44,46 @@ def summarize_alert_main(recv_q: Queue[Union[Alert, str]], send_q: Queue[str], n
     if not path.exists(alert_dir_path):
         mkdir(alert_dir_path)
 
-    t = Thread(target=receive_alert, args=(recv_q,))
-    t.setDaemon(True)
+    # アラート受信スレッドとの通信用キュー
+    data_list: threadQueue[Any] = threadQueue()
+
+    t = Thread(target=receive_alert, args=(recv_q, data_list))
+    t.daemon = True
     t.start()
 
     while True:
-        if not data_list:
-            event.clear()   # data_listが空になると同時にreceiveで'end'がappendされたらデッドロックなる？
-            event.wait()    # data_listが空(if文の中に入る) -> receive_alert()の中でsetされると、wait()が終わる
+        if not data_list.empty():
+            temp = data_list.get()
+            if temp == 'end':
+                logger.info('receive end!!')
+                data_list.task_done()
+                break
 
-        temp = data_list.popleft()
-        if temp == 'end':
-            break
+            alert = cast(Alert, temp)
 
-        alert = cast(Alert, temp)
+            file_name = alert.file_name
+            content = alert.content
+            label = alert.label
 
-        file_name = alert.file_name
-        content = alert.content
-        label = alert.label
+            # label と content にnthを追加
+            label = 'Nth,' + label
+            content = str(nth) + ', ' + content
 
-        # label と content にnthを追加
-        label = 'Nth,' + label
-        content = str(nth) + ', ' + content
+            # "falsification.cysec.cs.ritsumei.ac.jp"がURLに含まれる場合、ファイル名を変更する
+            if ("falsification.cysec.cs.ritsumei.ac.jp" in alert.url) or ("192.168.0.233" in alert.url):
+                file_name = "TEST_" + file_name
 
-        # "falsification.cysec.cs.ritsumei.ac.jp"がURLに含まれる場合、ファイル名を変更する
-        if ("falsification.cysec.cs.ritsumei.ac.jp" in alert.url) or ("192.168.0.233" in alert.url):
-            file_name = "TEST_" + file_name
+            # label と content を出力
+            if file_name.endswith('.csv'):
+                if not path.exists(alert_dir_path + '/' + file_name):
+                    w_file(alert_dir_path + '/' + file_name, label + '\n', mode="a")
+                w_file(alert_dir_path + '/' + file_name, content + '\n', mode="a")
+            else:
+                w_file(alert_dir_path + '/' + file_name, content + '\n', mode="a")
 
-        # label と content を出力
-        if file_name.endswith('.csv'):
-            if not path.exists(alert_dir_path + '/' + file_name):
-                w_file(alert_dir_path + '/' + file_name, label + '\n', mode="a")
-            w_file(alert_dir_path + '/' + file_name, content + '\n', mode="a")
-        else:
-            w_file(alert_dir_path + '/' + file_name, content + '\n', mode="a")
+            data_list.task_done()
 
-    logger.info("Summarize_alert: end")
+    data_list.join()
+    logger.info("Summarize_alert: FIN")
 
     send_q.put('end')   # 親にendを知らせる
