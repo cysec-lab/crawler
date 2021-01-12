43a93a147f3e4481d24e14591bdc8f11a53fb78b
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-01-12 21:11:40 +0900

    Feat: iframe inspect twice

--- src/webdrivers/use_browser.py
+++ src/webdrivers/use_browser.py
@@ -132,13 +132,32 @@ def set_html(page: Page, driver: WebDriver, alert_process_q: Queue[Union[Crawler
                     iframe_url = iframe.get_attribute("data-src")
                 driver.switch_to.frame(iframe)
                 sleep(0.2)
+                logger.debug('switch to %s', str(iframe))
+                # iframe内のURLを修正して追記
                 iframe_html = complete_url_by_html(driver.page_source, iframe_url, page.html_special_char)
-                driver.switch_to.default_content()
-                # iframe内のURLを修正する
                 page.html += iframe_html
+
+                # iframe は2重までは調査
+                iframe_list2 = driver.find_elements_by_tag_name("iframe")
+                for if2 in iframe_list2:
+                    iframe_url2 = if2.get_attribute("src")
+                    if iframe_url2 == "":
+                        iframe_url2 = if2.get_attribute("data-src")
+                    logger.debug('add %s', str(if2))
+                    driver.switch_to.frame(if2)
+                    sleep(0.2)
+                    iframe_html = complete_url_by_html(driver.page_source, iframe_url2, page.html_special_char)
+                    page.html += iframe_html
+                    # もとのフレームまで戻る, トップから順に戻らないとエラーになる
+                    driver.switch_to.default_content()
+                    sleep(0.2)
+                    driver.switch_to.frame(iframe)
+                    sleep(0.2)
+
+                driver.switch_to.default_content()
                 sleep(0.2)
             except Exception as err:
-                logger.info(f"Failed to switch iframe, {err}")
+                logger.info(f"Failed to switch iframe: {iframe}, {err}")
                 alert_process_q.put(CrawlerAlert(
                     url       = page.url_initial,
                     file_name = 'iframe_switch_failed.csv',
